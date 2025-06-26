671921f236d7b3a3362c9c570088e7546462de66
sinan839 <sinanicru9@gmail.com>
2025-06-26 17:00:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:login/login.dart';
+import 'package:login/welcome.dart';
+
+void main() {
+  runApp(MaterialApp(home: welcome(),debugShowCheckedModeBanner: false,));
+}
---
+++ lib/welcome.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class welcome extends StatelessWidget {
+  const welcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+        child: Column(
+        children: [
+        SizedBox(height: 50),
+    Text(
+    "welcome",
+    style: TextStyle(fontSize: 70, color: Colors.black),
+    ),
+    SizedBox(height: 50),
+    Center(
+    child: CircleAvatar(
+    radius: 150,
+    backgroundImage: NetworkImage(
+    "https://image.winudf.com/v2/image1/Y29tLmFwcC50ZXN0cGl3aWtiYXNpY19zY3JlZW5fMF8xNTk3ODU2MzY1XzAwNg/screen-0.jpg?fakeurl=1&type=.jpg",
+    ),
+    ),
+    ),
+    SizedBox(height: 50),
+    SizedBox(
+    height: 50,
+    width: 500,
+    child: ElevatedButton(onPressed: () {}, child: Text("login"),style: ElevatedButton.styleFrom(side: BorderSide(color:Colors.black)),
+    ),
+    ),
+    SizedBox(height: 50),
+    SizedBox(
+    height: 50,
+    width: 500,
+    child: ElevatedButton(
+    onPressed: () {},
+    child: Text("sign up"),
+    style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.blue,
+    foregroundColor: Colors.white,
+    ),
+    ),
+    ),
+    ],
+    ),
+    ),
+    );
+  }
+}
